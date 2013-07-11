4b4df701e0ec5b228fb71f68746cb96e38bd07e4
David Huang <davidhuang715432279@gmail.com>
2013-07-11 08:47:50 +0800

    first commit

--- Boosttest/main.cpp
+++ Boosttest/main.cpp
@@ -9,13 +9,26 @@
 #include <iostream>
 #include <boost/asio.hpp>
 #include <boost/scoped_ptr.hpp>
+#include <boost/progress.hpp>
 #include <boost/lexical_cast.hpp>
+#include <boost/timer.hpp>
+
+using namespace boost;
+using namespace std;
 
 int main(int argc, const char * argv[])
 {
 
     // insert code here...
+    timer t;
+    progress_timer time;
+    cout << "max timespan:"<<t.elapsed_max()/3600<<"h"<<endl;
+    cout << "min timespan:"<<t.elapsed_min()<<"s"<<endl;
+
+
+    
     std::cout << "Hello, World!\n";
+    std::cout << "sdf";
     using boost::lexical_cast;
     int a = lexical_cast<int>("123");
     std::cout<< a << std::endl;
